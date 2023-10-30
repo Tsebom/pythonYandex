bc4b77623055ce8238158ee6bbc0637f2a59a54d
Нежданов Сергей <Tsebom@yandex.ru>
2023-10-30 23:11:47 +0400

    stringH

--- str.py
+++ str.py
@@ -1,6 +1,14 @@
 N = input()
-spt = N.split(' ')
 
-result = int(spt[0]) + int(spt[1])
+arr = []
 
-print(result)
+for i in range(int(N)):
+    n = input().find('зайка')
+
+    if n != -1:
+        arr.append(n + 1)
+    else:
+        arr.append('Заек нет =(')
+
+for i in range(len(arr)):
+    print(arr[i])
